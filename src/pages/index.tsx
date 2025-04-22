41cfde33fcc2d1a2be7a74d858e07be60eca051c
Zainab Oyedeji <53382230+zainaboyedeji@users.noreply.github.com>
2025-04-22 17:45:09 +0100

    cleanup

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,27 +1,50 @@
-import { Button } from '@/components/ui/button';
-import { Navbar } from '@/components/navbar';
-import WebPageTitle from '@/components/webpagetitle';
-import ServicesSection from '@/components/services-section';
-import Footer from '@/components/footer';
+"use client";
+
+import { useState, useEffect } from "react";
+import { Button } from "@/components/ui/button";
+import { Navbar } from "@/components/navbar";
+import WebPageTitle from "@/components/webpagetitle";
+import ServicesSection from "@/components/services-section";
+import Footer from "@/components/footer";
 
 export default function HeroSection() {
+  const headings = [
+    "One platform, all your services.",
+    "Seamless Services, Anytime, Anywhere.",
+    "Book Smart. Work Easy. Snapwork.",
+  ];
+
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentIndex((prev) => (prev + 1) % headings.length);
+    }, 3000);
+
+    return () => clearInterval(interval);
+  }, []);
+
   return (
     <>
       <WebPageTitle title="Landing Page | Snapwork" />
-      <div className='heroBackground'>
+      <div className="heroBackground">
         <Navbar />
         <main className="flex flex-col items-center mt-16 text-center px-4">
-          <h1 className="text-4xl md:text-5xl font-bold text-blue-100 mb-6 mt-10">
-            One platform, all your services
+          <h1
+            key={currentIndex}
+            className="text-4xl md:text-5xl font-bold text-blue-100 mb-6 mt-10 transition-opacity duration-1000 ease-in-out opacity-100"
+          >
+            {headings[currentIndex]}
           </h1>
+
           <Button className="bg-blue-100 text-blue-200 rounded-xl px-6 py-3">
             Get started with us
           </Button>
         </main>
       </div>
 
-      <ServicesSection/>
-      <Footer/>
+      <ServicesSection />
+      <Footer />
     </>
   );
 }
