2829983c6e498ea937d92b6ee841dc7496657bd7
Zainab Oyedeji <53382230+zainaboyedeji@users.noreply.github.com>
2025-09-26 21:17:18 +0100

    cleanup

--- src/components/quote.tsx
+++ src/components/quote.tsx
@@ -24,12 +24,12 @@ export default function Quote() {
   return (
     <section className="w-full min-h-screen bg-gradient-to-br from-blue-50 to-indigo-100 spiral-edges overflow-hidden">
       <div className="grid md:grid-cols-2 items-center max-w-full">
-        <div className="space-y-6 px-4 md:px-20 pt-20 md:pt-40 lg:pt-12">
+        <div className="space-y-6 px-4 md:px-20 pt-48 md:pt-40 lg:pt-12">
           <p className="text-3xl font-medium text-darkblue">
             Dependable partners for every business need from{" "}
           </p>
 
-          <h2 className="text-4xl md:text-7xl font-bold text-[#162B7D] transition-all duration-500">
+          <h2 className="text-4xl md:text-7xl font-bold text-darkblue transition-all duration-500">
             {text}
             <div className="mt-2">
               <Icon name="underline" color={color} key={text} />
