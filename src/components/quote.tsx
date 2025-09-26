93f26602e430af28f9792baed2530d3c70a0b286
Zainab Oyedeji <53382230+zainaboyedeji@users.noreply.github.com>
2025-09-26 21:04:35 +0100

    get a quote

--- src/components/quote.tsx
+++ src/components/quote.tsx
@@ -22,36 +22,32 @@ export default function Quote() {
   const { text, color } = rotatingWords[index];
 
   return (
-    <section
-      className="w-full min-h-screen bg-gradient-to-br from-blue-50 to-indigo-100 spiral-edges"
-
-    >
-      <div className="grid md:grid-cols-2 items-center">
-        <div className="space-y-6 px-20 pt-40 lg:pt-12">
+    <section className="w-full min-h-screen bg-gradient-to-br from-blue-50 to-indigo-100 spiral-edges overflow-hidden">
+      <div className="grid md:grid-cols-2 items-center max-w-full">
+        <div className="space-y-6 px-4 md:px-20 pt-40 lg:pt-12">
           <p className="text-3xl font-medium text-darkblue">
-            Does your business need <br />
-            reliable partners like
+            Dependable partners for every business need from{" "}
           </p>
 
-          <h2 className="text-7xl font-bold text-[#162B7D] transition-all duration-500">
+          <h2 className="text-4xl md:text-7xl font-bold text-[#162B7D] transition-all duration-500">
             {text}
             <div className="mt-2">
               <Icon name="underline" color={color} key={text} />
             </div>
           </h2>
 
-          <p className="text-2xl text-darkblue">we&apos;ve got you covered</p>
+          <p className="text-2xl text-darkblue">you can trust.</p>
 
-          <Button>Get a quote</Button>
+          <Button>Request a Quote.</Button>
         </div>
 
-        <div className="mt-12 md:mt-0 flex justify-end">
+        <div className="mt-12 md:mt-0 flex justify-end overflow-hidden">
           <Image
             src="/images/hand-phone.svg"
             alt="Phone in hand"
             width={400}
             height={400}
-            className="h-auto max-w-[90%] md:max-w-none md:w-auto md:translate-x-16 animate-float rotate-10"
+            className="h-auto w-full max-w-[300px] md:max-w-[400px] animate-float rotate-10"
           />
         </div>
       </div>
