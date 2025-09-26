d10ecf281674bb7f640a23a2e9464a6e64dcef7d
Zainab Oyedeji <53382230+zainaboyedeji@users.noreply.github.com>
2025-09-26 03:54:21 +0100

    our services

--- src/components/services-section.tsx
+++ src/components/services-section.tsx
@@ -3,17 +3,18 @@ import Icon from "./icon";
 
 const services = [
   { title: "Inverter Repair & install", icon: "inverter-repair" },
-  { title: "Plumbing", icon: "plumbing" },
+  { title: "Fashion Designer", icon: "fashion-designer" },
+
+  { title: "Electrical Services", icon: "electrical-services" },
+  { title: "Hair Services", icon: "hair-services" },
+
   { title: "Generator Repair & install", icon: "generator-repair" },
-  { title: "Makeup Services", icon: "makeup-services" },
   { title: "Laundry & Dry Cleaning", icon: "laundry-drycleaning" },
   { title: "Cleaning Services", icon: "cleaning-services" },
+  { title: "Makeup Services", icon: "makeup-services" },
+
   { title: "Catering Services", icon: "catering-services" },
-  { title: "Hair Services", icon: "hair-services" },
-  { title: "Fashion Designer", icon: "fashion-designer" },
-  { title: "Electrical Services", icon: "electrical-services" },
-  { title: "Fashion Designer", icon: "fashion-designer" },
-  { title: "Electrical Services", icon: "electrical-services" },
+  { title: "Plumbing", icon: "plumbing" },
 ];
 
 export default function ServicesPage() {
@@ -21,25 +22,23 @@ export default function ServicesPage() {
     <div className="min-h-screen py-16 px-4">
       <h2 className="text-6xl font-bold text-center">Our Services</h2>
       <div className="flex justify-center mb-10">
-      <Icon name="underline" color="stroke-orange-100" />
-
+        <Icon name="underline" color="stroke-orange-100" />
       </div>
 
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-x-10 gap-y-12 px-4 max-w-6xl mx-auto">
-      {services.map((service, index) => (
+        {services.map((service, index) => (
           <div
-          key={index}
-          className="bg-[#2A3D8F] rounded-xl px-6 py-6 flex items-center justify-between shadow-md w-full"
-        >
-          <div className="basis-[40%] flex justify-center">
-            <Icon name={service.icon} />
+            key={index}
+            className="bg-blue-400 rounded-xl px-6 py-6 flex items-center justify-between shadow-md w-full"
+          >
+            <div className="basis-[40%] flex justify-center">
+              <Icon name={service.icon} />
+            </div>
+
+            <p className="basis-[40%] font-semibold text-lg leading-tight text-center">
+              {service.title}
+            </p>
           </div>
-        
-          <p className="basis-[40%] font-semibold text-lg leading-tight text-center">
-            {service.title}
-          </p>
-        </div>
-        
         ))}
       </div>
     </div>
