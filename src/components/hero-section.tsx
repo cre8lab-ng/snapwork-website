cae0ff38347a800f2bcc80f2ff5a3e9e9a5acaa9
Zainab Oyedeji <53382230+zainaboyedeji@users.noreply.github.com>
2025-09-26 02:49:04 +0100

    hero-section

--- src/components/hero-section.tsx
+++ src/components/hero-section.tsx
@@ -3,33 +3,38 @@ import Image from "next/image";
 
 export default function HeroSection() {
   return (
-    <div className="heroBackground w-full min-h-screen">
-      <main className="flex flex-col items-center text-center px-4 pt-6">
-        <Image
-          src="/images/snapwork-logo.svg"
-          alt="Snapwork Logo"
-          width={250}
-          height={250}
-        />
-        <div className="mt-20 text-darkblue">
-          <h3 className="text-5xl md:text-6xl font-bold leading-tight">
-            One Platform, <br />
-            All your services
-          </h3>
-          <p className="mt-10 text-md md:text-2xl font-medium">
-            Easily get matched with trusted <br />
-            professionals whether you need
-          </p>
+    <div className="w-full min-h-screen">
+      <main className="flex flex-col items-center text-center px-10 pt-6">
+        <div className="relative w-full">
+          <Image
+            src="/images/hero-illustration-workers.svg"
+            alt="Snapwork Logo"
+            width={800}
+            height={600}
+            className="w-full rounded-lg"
+          />
+          <div className="absolute inset-0 flex flex-col items-center justify-start pt-26 text-center px-4 text-white">
+            <h3 className="text-5xl md:text-6xl font-bold leading-tight drop-shadow-lg">
+              One Platform, <br />
+              All your services
+            </h3>
+            <p className="mt-10 text-md md:text-2xl font-medium drop-shadow-lg">
+              Easily get matched with trusted <br />
+              professionals whether you need
+            </p>
 
-          <div className="flex justify-center mt-6">
-            <a
-              href="https://book-snapwork.netlify.app/"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="hidden md:block"
-            >
-              <Button variant="primary">Book Now</Button>
-            </a>
+            <div className="flex justify-center mt-6 cursor-pointer">
+              <a
+                href="https://book-snapwork.netlify.app/"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="hidden md:block"
+              >
+                <Button variant="secondary" size="lg">
+                  Book Now
+                </Button>
+              </a>
+            </div>
           </div>
         </div>
       </main>
--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -19,7 +19,7 @@ const variants = {
 const sizes = {
   sm: "px-3 py-1.5 text-sm",
   md: "px-4 py-2 text-base",
-  lg: "px-6 py-3 text-lg",
+  lg: "px-12 py-2 text-lg rounded-full",
 };
 
 export const Button = forwardRef<HTMLButtonElement, ButtonProps>(
